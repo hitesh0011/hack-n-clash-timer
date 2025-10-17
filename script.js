3bd1a7ded15d2d155e0dddc6e99bd7a598b9b9f3
hitesh0011 <110589858+hitesh0011@users.noreply.github.com>
2025-10-17 16:46:30 +0530

    fixed script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // let totalTime = 5;// Test time in seconds (replace with 2*60*60 for 2 hours)
-let totalTime = 2 * 2 * 60; // 2 hours in seconds
+let totalTime = 2 * 60 * 60; // 2 hours in seconds
 let remainingTime = totalTime;
 let timerInterval = null;
 let isRunning = false;
@@ -73,3 +73,4 @@ resetBtn.addEventListener('click', () => {
 // Initialize
 
 updateDisplay();
+
